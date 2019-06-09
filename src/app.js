8be213e200c2a725c80c63dd589d155b82064535
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-06-09 13:58:58 +0300

    Add WS

--- src/app.js
+++ src/app.js
@@ -5,12 +5,42 @@ const config = require("config");
 const cors = require('cors');
 const app = express();
 
-app.use(express.static('public'));
-app.use('/api', 
-        bodyParser.json(),
-        cors(), 
-        require('./router'));
+// app.use(express.static('public'));
+// app.use('/api', 
+//         bodyParser.json(),
+//         cors(), 
+//         require('./router'));
 
-app.listen(config.server.port, () => {
-    console.log(`app started at port ${config.server.port}`);
+// app.listen(config.server.port, () => {
+//     console.log(`app started at port ${config.server.port}`);
+// });
+
+var WebSocketServer = new require('ws');
+
+// подключенные клиенты
+var clients = {};
+
+// WebSocket-сервер на порту 8081
+var webSocketServer = new WebSocketServer.Server({
+  port: config.server.port
+});
+webSocketServer.on('connection', function(ws) {
+
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("новое соединение " + id);
+
+  ws.on('message', function(message) {
+    console.log('получено сообщение ' + message);
+
+    for (var key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    console.log('соединение закрыто ' + id);
+    delete clients[id];
+  });
+
 });
